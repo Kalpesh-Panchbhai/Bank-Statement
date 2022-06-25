b715d896bfb3c153983c8a63c8fbe2f6ee7323ce
kalpeshpanchbhai <kalpesh.panch90@gmail.com>
2022-06-25 10:32:38 +0530

    Few changes

--- src/main/java/com/kalpesh/bankstatement/model/Mode.java
+++ src/main/java/com/kalpesh/bankstatement/model/Mode.java
@@ -4,24 +4,24 @@ import java.util.Arrays;
 
 public enum Mode {
 
-	DEBIT_CARD("DEBIT CARD"), NET_BANKING("NET BANKING"), ICICI_ATM("ICICI ATM");
+    DEBIT_CARD("DEBIT CARD"), NET_BANKING("NET BANKING"), ICICI_ATM("ICICI ATM"), OTHER_ATMS("OTHER ATMS"), MOBILE_BANKING("MOBILE BANKING"), UPI("UPI"), ATM("ATM"), OTHER("OTHER");
 
-	private final String mode;
+    private final String mode;
 
-	Mode(String mode) {
-		this.mode = mode;
-	}
+    Mode(String mode) {
+        this.mode = mode;
+    }
 
-	public static boolean contains(String mode) {
-		for (Mode mode1 : Mode.values()) {
-			if (mode1.mode.equals(mode)) {
-				return true;
-			}
-		}
-		return false;
-	}
+    public static boolean contains(String mode) {
+        for (Mode mode1 : Mode.values()) {
+            if (mode1.mode.equals(mode)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	public static Mode value(String mode) {
-		return Arrays.stream(Mode.values()).filter(mode1 -> mode1.mode.equals(mode)).findFirst().get();
-	}
+    public static Mode value(String mode) {
+        return Arrays.stream(Mode.values()).filter(mode1 -> mode1.mode.equals(mode)).findFirst().get();
+    }
 }
